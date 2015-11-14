d70c55b1e7af1e8d9dcfb968d74d024260c374d3
twxl1993 <twxl1993@gmail.com>
2015-11-15 00:32:45 +0800

    delete package ppp

--- package/ppp/files/ppp.sh
+++ package/ppp/files/ppp.sh
@@ -138,7 +138,6 @@ ppp_generic_setup() {
 		ip-down-script /lib/netifd/ppp-down \
 		ipv6-down-script /lib/netifd/ppp-down \
 		${mtu:+mtu $mtu mru $mtu} \
-		$([ "$(uci get macvlan.config.enabled)" == "1" ] && echo syncppp $(cat /etc/config/network | grep -c "proto 'pppoe'")) \
 		"$@" $pppd_options
 }
 
